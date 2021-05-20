989539ea3a07f5f7bba6994d8d35fd58f64f88c5
Steve Jackson <steve@redlabsoftware.com>
2021-05-20 14:42:45 +0100

    add ardanlabs config to startup

--- app/products-api/main.go
+++ app/products-api/main.go
@@ -1,5 +1,65 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ardanlabs/conf"
+	"github.com/pkg/errors"
+)
+
+const (
+	ApiName = "PRODUCTS"
+)
+
+// version number of the program - to be set in part of ci/cd pipeline
+var build = "develop"
+
 func main() {
-	println("products-api")
+	log := log.New(os.Stdout, "PRODUCTS : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	if err := run(log); err != nil {
+		log.Println("main: error:", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *log.Logger) error {
+
+	// ===
+	// Configuration
+	var cfg struct {
+		conf.Version
+		Web struct {
+			APIHost         string `conf:"default:0.0.0.0:3000"`
+			ReadTimeout     string `conf:"default:5s"`
+			WriteTimeout    string `conf:"default:5s"`
+			ShutdownTimeout string `conf:"default:5s"`
+		}
+	}
+	cfg.SVN = build
+	cfg.Version.Desc = "products api"
+
+	if err := conf.Parse(os.Args[1:], ApiName, &cfg); err != nil {
+		switch err {
+		case conf.ErrHelpWanted:
+			usage, err := conf.Usage(ApiName, &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		case conf.ErrVersionWanted:
+			version, err := conf.VersionString(ApiName, &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config version")
+			}
+			fmt.Println(version)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/stevejxn/learn-go-webapi
 
 go 1.16
+
+require (
+	github.com/ardanlabs/conf v1.4.0
+	github.com/pkg/errors v0.9.1
+)
